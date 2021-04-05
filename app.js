ffcc1446c29f8a05bba664a9f80a7e296395966e
prajwlshettycode <81403420+prajwlshettycode@users.noreply.github.com>
2021-04-05 11:03:47 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-var userName = prompt("give me a name");
+var btnTranslate = document.querySelector("#btn-translator");
+var txtInput = document.querySelector("#btn-input");
+var outputDiv = document.querySelector("#output");
 
-alert(userName);
+
+
+function clickHandler() {
+    outputDiv.innerText = 'Prajwal'
+
+};
+ 
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
