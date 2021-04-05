28353e3d883720b70e2edb3c039e2f7597665114
prajwlshettycode <81403420+prajwlshettycode@users.noreply.github.com>
2021-04-05 22:00:06 +0530

    added design elements

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translator");
-var txtInput = document.querySelector("#btn-input");
+var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverUrRL = "https://api.funtranslations.com/translate/minion.json"
 
+function translationURL(input){
+    return serverUrRL+"?"+"text="+input 
+}
 
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Error Occurred in Server, Please try again sometime later!")
+}
 
 function clickHandler() {
-    outputDiv.innerText = 'Prajwal'
+    var inputTxt = txtInput.value;
 
+    //calling server
+    fetch(translationURL(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+            var translatedOutput = json.contents.translated;
+            outputDiv.innerText = translatedOutput;
+    }).catch(errorHandler)
 };
  
 btnTranslate.addEventListener("click", clickHandler)
